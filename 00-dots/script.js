36375e02e66b65ecfc11a3cfd086610e26e52887
Adam Pearce <adampearce@google.com>
2020-03-05 00:40:22 -0500

    dots work

--- 00-dots/script.js
+++ 00-dots/script.js
@@ -4,7 +4,11 @@ console.clear()
 var c = d3.conventions({
   sel: d3.select('#graph').html(''),
   height: 800,
+  margin: {right: 50},
+  layers: 'cs'
 })
+
+var ctx = c.layers[0]
 
 var colWidth = c.width/6
 var cellWidth = d3.scaleLinear().domain([0,6457]).range([0, colWidth])
@@ -184,7 +188,7 @@ var paths = [
     y: [0, 1],
     v: 2934,
     o: [-cellWidth(4966) / 2 + cellWidth(2934) / 2, 0],
-    tie: "SAK-DNA"
+    id: "SAK-DNA"
   },
   {
     // SAK -> No DNA
@@ -192,7 +196,7 @@ var paths = [
     y: [0, 1],
     v: 2032,
     o: [-cellWidth(4966) / 2 + cellWidth(2934) + cellWidth(2032 / 2), 0],
-    tie: "SAK-No DNA"
+    id: "SAK-No DNA"
   },
   {
     // DNA -> DNA Hit
@@ -200,7 +204,7 @@ var paths = [
     y: [1, 2],
     v: 1935,
     o: [-cellWidth(2934) / 2 + cellWidth(1935 / 2), 0],
-    tie: "DNA-DNA Hit"
+    id: "DNA-DNA Hit"
   },
   {
     // DNA -> No DNA Hit
@@ -208,7 +212,7 @@ var paths = [
     y: [1, 2],
     v: 999,
     o: [-cellWidth(2934) / 2 + cellWidth(1935) + cellWidth(999 / 2), 0],
-    tie: "DNA-No DNA Hit"
+    id: "DNA-No DNA Hit"
   },
   {
     // DNA Hit -> Invstigation
@@ -216,7 +220,7 @@ var paths = [
     y: [2, 3.5],
     v: 1935,
     o: [0, -cellWidth(6457 / 2) + cellWidth(1935 / 2)],
-    tie: "DNA Hit-Investigation"
+    id: "DNA Hit-Investigation"
   },
   {
     // No DNA Hit -> Investigation
@@ -224,7 +228,7 @@ var paths = [
     y: [2, 3.5],
     v: 999,
     o: [0, -cellWidth(6457 / 2) + cellWidth(1935) + cellWidth(999 / 2)],
-    tie: "No DNA Hit-Investigation"
+    id: "No DNA Hit-Investigation"
   },
   {
     // No DNA -> Invesitgation
@@ -232,7 +236,7 @@ var paths = [
     y: [1, 3.5],
     v: 2032,
     o: [0, -cellWidth(6457 / 2) + cellWidth(1935) + cellWidth(999) + cellWidth(2032 / 2)],
-    tie: "No DNA-Investigation"
+    id: "No DNA-Investigation"
   },
   {
     // Investigation -> Investigation in progress
@@ -240,7 +244,7 @@ var paths = [
     y: [3.5, 4.5],
     v: 4003,
     o: [-cellWidth(6457 / 2) + cellWidth(2454) + cellWidth(4003 / 2), 0],
-    tie: "Investigation-Investigation in progress"
+    id: "Investigation-Investigation in progress"
   },
   {
     // Investigation -> Investigation completed
@@ -248,7 +252,7 @@ var paths = [
     y: [3.5, 4.5],
     v: 2454,
     o: [-cellWidth(6457/2)+cellWidth(2454/2), 0],
-    tie: "Investigation-Investigation completed"
+    id: "Investigation-Investigation completed"
   },
   {
     // I.C. -> C not Ind
@@ -256,7 +260,7 @@ var paths = [
     y: [4.5, 5.5],
     v: 1831,
     o: [-cellWidth(2454 / 2) + cellWidth(623) + cellWidth(1831 / 2), 0],
-    tie: "Investigation completed-Case not indicted"
+    id: "Investigation completed-Case not indicted"
   },
   {
     // I.C. -> CI
@@ -264,7 +268,7 @@ var paths = [
     y: [4.5, 5.5],
     v: 623,
     o: [-cellWidth(2454 / 2) + cellWidth(623 / 2), 0],
-    tie: "Investigation completed-Case indicted"
+    id: "Investigation completed-Case indicted"
   },
   {
     // C.I. -> D in Prog.
@@ -272,7 +276,7 @@ var paths = [
     y: [5.5, 6.5],
     v: 342,
     o: [-cellWidth(623 / 2) + cellWidth(281) + cellWidth(342 / 2), 0],
-    tie: "Case indicted-Disposition in progress"
+    id: "Case indicted-Disposition in progress"
   },
   {
     // C.I. -> C.D.
@@ -280,7 +284,7 @@ var paths = [
     y: [5.5, 6.5],
     v: 281,
     o: [-cellWidth(623 / 2) + cellWidth(281 / 2), 0],
-    tie: "Case indicted-Case disposed"
+    id: "Case indicted-Case disposed"
   },
   {
     // C.D. -> Trial
@@ -288,7 +292,7 @@ var paths = [
     y: [6.5, 7.5],
     v: 52,
     o: [-cellWidth(281 / 2) + cellWidth(52 / 2), 0],
-    tie: "Case disposed-Trial"
+    id: "Case disposed-Trial"
   },
   {
     // Trial -> Guilty
@@ -296,7 +300,7 @@ var paths = [
     y: [7.5, 8.5],
     v: 34,
     o: [-cellWidth(52 / 2) + cellWidth(34 / 2), 0],
-    tie: "Trial-Guilty"
+    id: "Trial-Guilty"
   },
   {
     // Trial -> Not Guilty
@@ -304,7 +308,7 @@ var paths = [
     y: [7.5, 8.5],
     v: 18,
     o: [-cellWidth(52 / 2) + cellWidth(34) + cellWidth(18 / 2), 0],
-    tie: "Trial-Not Guilty"
+    id: "Trial-Not Guilty"
   },
   {
     // C.D. -> Plea
@@ -312,7 +316,7 @@ var paths = [
     y: [6.5, 7.5],
     v: 180,
     o: [-cellWidth(281 / 2) + cellWidth(52) + cellWidth(180 / 2), 0],
-    tie: "Case diposed-Plea"
+    id: "Case diposed-Plea"
   },
   {
     // C.D. -> D.R.
@@ -323,7 +327,7 @@ var paths = [
       -cellWidth(281 / 2) + cellWidth(52) + cellWidth(180) + cellWidth(7 / 2),
       0
     ],
-    tie: "Case diposed-Dismissed, reindicted"
+    id: "Case diposed-Dismissed, reindicted"
   },
   {
     // C.D. -> D w/o P
@@ -331,7 +335,7 @@ var paths = [
     y: [6.5, 7.5],
     v: 28,
     o: [-cellWidth(281 / 2) + cellWidth(52) + cellWidth(180) + cellWidth(7) + cellWidth(28 / 2), 0],
-    tie: "Case diposed-Dismissed w/o prejudice"
+    id: "Case diposed-Dismissed w/o prejudice"
   },
   {
     // C.D. -> D w/ P
@@ -339,11 +343,67 @@ var paths = [
     y: [6.5, 7.5],
     v: 14,
     o: [-cellWidth(281 / 2) + cellWidth(52) + cellWidth(180) + cellWidth(7) + cellWidth(28) + cellWidth(14 / 2), 0],
-    tie: "Case diposed-Dismissed w prejudice"
+    id: "Case diposed-Dismissed w prejudice"
   }
 ]
 
-var data = d3.range(200)
+
+var id2Path = {}
+paths.forEach(d => {
+  id2Path[d.id] = d
+  d.inId = d.id.split('-')[0]
+  d.outId = d.id.split('-')[1]
+})
+
+var id2Node = {}
+var nodes = _.uniq(paths.map(d => d.inId)).map(id => {
+  var inPaths = paths.filter(d => d.outId == id)
+  var outPaths = paths.filter(d => d.inId == id)
+
+  var domain = []
+  var sum = 0
+  outPaths.forEach(d => {
+    sum += d.v
+    domain.push(sum)
+  })
+
+  var threshold = d3.scaleThreshold()
+    .domain(domain.map(d => d/sum))
+    .range(outPaths)
+
+  function randomChildPath(){
+    return threshold(Math.random())
+  }
+
+  return id2Node[id] = {id, inPaths, outPaths, randomChildPath}
+})
+
+paths.forEach(d => {
+  d.inNode = id2Node[d.inId]
+  d.outNode = id2Node[d.outId]
+})
+
+
+var points = d3.range(4966).map(i => {
+  var speed = (Math.random() + .5)/240
+  var wait = -Math.random()*50
+  var xOffset = Math.random() - .5
+  var y = 0
+
+  var rv = {paths: [], i, speed, wait, xOffset}
+  var curNode = id2Node['SAK']
+
+  var j = 0
+  while (curNode && j < 10){
+    j++
+
+    var path = curNode.randomChildPath()
+    curNode = path.outNode
+    rv.paths.push(path)
+  }
+
+  return rv
+})
 
 var cellHeight = 15
 c.x.domain([0, 5])
@@ -355,6 +415,16 @@ paths.forEach(calcPath)
 
 var pathSel = c.svg.appendMany('path.connector', paths)
   .at({d: d => d.path, strokeWidth: d => d.width})
+  .each(function(d){
+    var length = this.getTotalLength()
+    var pctCache = {}
+    d.calcPos = pct => {
+      pct = Math.round(pct*100)/100
+      if (pctCache[pct]) return pctCache[pct]
+
+      return pctCache[pct] = this.getPointAtLength(pct*length)
+    }
+  })
 
 var rowSel = c.svg.appendMany('g.row', outline)
   .translate(d => c.y(d.y), 1)
@@ -366,13 +436,39 @@ nodeSel.append('rect')
   .at({
     x: d => -cellWidth(d.value/2),
     width: d => cellWidth(d.value),
-    height: cellHeight
+    height: cellHeight,
+    fillOpacity: d => d.label == 'Rape kit test' ? 1 : 0 
   })
+
 
 nodeSel.append('text')
   .text(d => d.label)
   .at({textAnchor: 'middle', fontSize: 12, y: -5})
 
+
+var tickIndex = 0
+if (window.__dottimer) window.__dottimer.stop()
+window.__dottimer = d3.timer(t => {
+  ctx.clearRect(-c.margin.left, -c.margin.right, c.totalWidth, c.totalWidth)
+
+  points.forEach(d => {
+    var t = tickIndex*d.speed + d.wait
+    if (t < 0 || t > d.paths.length) return
+
+    var path = d.paths[Math.floor(t)]
+
+    var pos = path.calcPos(t % 1)
+    ctx.beginPath()
+    ctx.arc(pos.x + d.xOffset*path.width, pos.y, 3, 0, 2*Math.PI)
+    ctx.fill()
+    ctx.stroke()
+  })
+
+
+
+
+  tickIndex++
+})
 
 
 
